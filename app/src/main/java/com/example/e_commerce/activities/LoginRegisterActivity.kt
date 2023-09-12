72fc42b0a1a79110f4a3ea16373426f69ed10123
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-09-12 14:52:18 +0530

    basic setup

---
+++ app/src/main/java/com/example/e_commerce/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.e_commerce.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.e_commerce.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
--- app/src/main/java/com/example/e_commerce/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/e_commerce/activities/ShoppingActivity.kt
@@ -1,11 +1,12 @@
-package com.example.e_commerce
+package com.example.e_commerce.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.e_commerce.R
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
     }
 }
