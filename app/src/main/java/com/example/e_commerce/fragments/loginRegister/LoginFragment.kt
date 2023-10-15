f3b850629ff23e1cebcc071e8c62b9bfccd86cde
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-10-15 15:14:07 +0530

    Login accounts

--- app/src/main/java/com/example/e_commerce/fragments/loginRegister/LoginFragment.kt
+++ app/src/main/java/com/example/e_commerce/fragments/loginRegister/LoginFragment.kt
@@ -1,7 +1,66 @@
 package com.example.e_commerce.fragments.loginRegister
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.e_commerce.R
+import com.example.e_commerce.activities.ShoppingActivity
+import com.example.e_commerce.databinding.FragmentLoginBinding
+import com.example.e_commerce.util.Resource
+import com.example.e_commerce.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginFragment: Fragment(R.layout.fragment_login) {
+    private lateinit var binding: FragmentLoginBinding
+    private val viewModel by viewModels<LoginViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            buttonLoginLogin.setOnClickListener {
+                val email = etEmailLogin.text.toString().trim()
+                val password = etPasswordLogin.text.toString()
+                viewModel.login(email, password)
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.login.collect {
+                when(it) {
+                    is Resource.Loading -> {
+                        binding.buttonLoginLogin.startAnimation()
+                    }
+                    is Resource.Success -> {
+                        binding.buttonLoginLogin.revertAnimation()
+                        Intent(requireActivity(), ShoppingActivity::class.java).also { intent->
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+                        }
+                    }
+                    is Resource.Failure -> {
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                        binding.buttonLoginLogin.revertAnimation()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
 }
