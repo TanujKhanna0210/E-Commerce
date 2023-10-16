a1ac34e6aa805e396fa7108a16d7dd0a22fed6f9
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-10-17 00:01:41 +0530

    Setting up bottom navigation

--- app/src/main/java/com/example/e_commerce/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/e_commerce/activities/ShoppingActivity.kt
@@ -2,11 +2,21 @@ package com.example.e_commerce.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.e_commerce.R
+import com.example.e_commerce.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityShoppingBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.shoppingHostFragment)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
