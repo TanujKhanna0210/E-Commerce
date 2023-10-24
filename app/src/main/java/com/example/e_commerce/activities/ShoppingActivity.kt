ed8f70464f99922fbd99c132a4f307771f9a3729
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-10-24 22:05:25 +0530

    Displaying the special products on mainFragment

--- app/src/main/java/com/example/e_commerce/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/e_commerce/activities/ShoppingActivity.kt
@@ -6,7 +6,9 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.e_commerce.R
 import com.example.e_commerce.databinding.ActivityShoppingBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ShoppingActivity : AppCompatActivity() {
 
     val binding by lazy {
--- app/src/main/java/com/example/e_commerce/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/com/example/e_commerce/fragments/categories/MainCategoryFragment.kt
@@ -1,7 +1,78 @@
 package com.example.e_commerce.fragments.categories
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.e_commerce.R
+import com.example.e_commerce.adapters.SpecialProductsAdapter
+import com.example.e_commerce.databinding.FragmentMainCategoryBinding
+import com.example.e_commerce.util.Resource
+import com.example.e_commerce.viewmodel.MainCategoryViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+private const val TAG = "MainCategoryFragment"
+@AndroidEntryPoint
 class MainCategoryFragment: Fragment(R.layout.fragment_main_category) {
+    private lateinit var binding: FragmentMainCategoryBinding
+    private lateinit var specialProductsAdapter: SpecialProductsAdapter
+    private val viewModel by viewModels<MainCategoryViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainCategoryBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupSpecialProductRv()
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.specialProducts.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        showLoading()
+                    }
+                    is Resource.Success -> {
+                        specialProductsAdapter.differ.submitList(it.data)
+                        hideLoading()
+                    }
+                    is Resource.Failure -> {
+                        hideLoading()
+                        Log.e(TAG, it.message.toString())
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
+
+    private fun hideLoading() {
+        binding.mainCategoryProgressBar.visibility = View.GONE
+    }
+
+    private fun showLoading() {
+        binding.mainCategoryProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupSpecialProductRv() {
+        specialProductsAdapter = SpecialProductsAdapter()
+        binding.rvSpecialProducts.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = specialProductsAdapter
+        }
+    }
 }
