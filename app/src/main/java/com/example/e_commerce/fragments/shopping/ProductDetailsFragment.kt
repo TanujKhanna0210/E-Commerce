606ea6214a489b42eedacf860d1344a2aafd1530
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-11-02 23:19:19 +0530

    Add product to cart

--- app/src/main/java/com/example/e_commerce/fragments/shopping/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/e_commerce/fragments/shopping/ProductDetailsFragment.kt
@@ -4,22 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.e_commerce.R
 import com.example.e_commerce.adapters.ColorsAdapter
 import com.example.e_commerce.adapters.SizesAdapter
 import com.example.e_commerce.adapters.ViewPager2ImagesAdapter
+import com.example.e_commerce.data.CartProduct
 import com.example.e_commerce.databinding.FragmentProductDetailsBinding
+import com.example.e_commerce.util.Resource
 import com.example.e_commerce.util.hideBottomNavigationView
+import com.example.e_commerce.viewmodel.DetailsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class ProductDetailsFragment: Fragment() {
     private val args by navArgs<ProductDetailsFragmentArgs>()
     private lateinit var binding: FragmentProductDetailsBinding
     private val viewPagerAdapter by lazy { ViewPager2ImagesAdapter() }
     private val sizesAdapter by lazy { SizesAdapter() }
     private val colorsAdapter by lazy { ColorsAdapter() }
+    private var selectedColor: Int ?= null
+    private var selectedSizes: String ?= null
+    private val viewModel by viewModels<DetailsViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,6 +55,38 @@ class ProductDetailsFragment: Fragment() {
 
         binding.imageClose.setOnClickListener {
             findNavController().navigateUp()
+        }
+
+        sizesAdapter.onItemClick = {
+            selectedSizes = it
+        }
+
+        colorsAdapter.onItemClick = {
+            selectedColor = it
+        }
+
+        binding.buttonAddToCart.setOnClickListener {
+            viewModel.addUpdateProductInCart(CartProduct(product, 1, selectedColor, selectedSizes))
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.addToCart.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.buttonAddToCart.startAnimation()
+                    }
+                    is Resource.Success -> {
+                        binding.buttonAddToCart.revertAnimation()
+                        binding.buttonAddToCart.setBackgroundColor(resources.getColor(R.color.black))
+                    }
+                    is Resource.Failure -> {
+                        binding.buttonAddToCart.stopAnimation()
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+
+                }
+            }
         }
 
         binding.apply {
