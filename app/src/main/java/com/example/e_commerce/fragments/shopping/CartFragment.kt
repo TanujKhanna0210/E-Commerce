169563faed3169efe7f7542a5aca5f4f53ee1dd6
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-11-04 19:06:31 +0530

    Setting up cart fragment

--- app/src/main/java/com/example/e_commerce/fragments/shopping/CartFragment.kt
+++ app/src/main/java/com/example/e_commerce/fragments/shopping/CartFragment.kt
@@ -1,7 +1,111 @@
 package com.example.e_commerce.fragments.shopping
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.e_commerce.R
+import com.example.e_commerce.adapters.CartProductAdapter
+import com.example.e_commerce.databinding.FragmentCartBinding
+import com.example.e_commerce.util.Resource
+import com.example.e_commerce.util.VerticalItemDecoration
+import com.example.e_commerce.viewmodel.CartViewModel
+import kotlinx.coroutines.flow.collectLatest
 
 class CartFragment: Fragment(R.layout.fragment_cart) {
+
+    private lateinit var binding: FragmentCartBinding
+    private val cartAdapter by lazy { CartProductAdapter() }
+    private val viewModel by activityViewModels<CartViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCartBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupCartRv()
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.productsPrice.collectLatest { price ->
+                price ?. let {
+                    binding.tvTotalPrice.text = "$ $price"
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.cartProducts.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.progressBarCart.visibility = View.VISIBLE
+                    }
+                    is Resource.Success -> {
+                        binding.progressBarCart.visibility = View.INVISIBLE
+                        if (it.data!!.isEmpty()) {
+                            showEmptyCart()
+                            hideOtherViews()
+                        } else {
+                            hideEmptyCart()
+                            showOtherViews()
+                            cartAdapter.differ.submitList(it.data)
+                        }
+                    }
+                    is Resource.Failure -> {
+                        binding.progressBarCart.visibility = View.INVISIBLE
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+    }
+
+    private fun showOtherViews() {
+        binding.apply {
+            rvCart.visibility = View.VISIBLE
+            totalBoxContainer.visibility = View.VISIBLE
+            buttonCheckout.visibility = View.VISIBLE
+        }
+    }
+
+    private fun hideOtherViews() {
+        binding.apply {
+            rvCart.visibility = View.GONE
+            totalBoxContainer.visibility = View.GONE
+            buttonCheckout.visibility = View.GONE
+        }
+    }
+
+    private fun hideEmptyCart() {
+        binding.apply {
+            layoutCartEmpty.visibility = View.GONE
+        }
+    }
+
+    private fun showEmptyCart() {
+        binding.apply {
+            layoutCartEmpty.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setupCartRv() {
+        binding.rvCart.apply {
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+            adapter = cartAdapter
+            addItemDecoration(VerticalItemDecoration())
+        }
+    }
 }
--- app/src/main/java/com/example/e_commerce/viewmodel/CartViewModel.kt
+++ app/src/main/java/com/example/e_commerce/viewmodel/CartViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.e_commerce.data.CartProduct
 import com.example.e_commerce.firebase.FirebaseCommon
+import com.example.e_commerce.helper.getProductPrice
 import com.example.e_commerce.util.Resource
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentSnapshot
@@ -11,6 +12,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -23,6 +25,21 @@ class CartViewModel @Inject constructor(
 
     private val _cartProducts = MutableStateFlow<Resource<List<CartProduct>>>(Resource.Unspecified())
     val cartProducts = _cartProducts.asStateFlow()
+
+    val productsPrice = cartProducts.map {
+        when (it) {
+            is Resource.Success -> {
+                calculatePrice(it.data!!)
+            }
+            else -> null
+        }
+    }
+
+    private fun calculatePrice(data: List<CartProduct>): Float {
+        return data.sumByDouble { cartProduct ->
+            (cartProduct.product.offerPercentage.getProductPrice(cartProduct.product.price) * cartProduct.quantity).toDouble()
+        }.toFloat()
+    }
 
     private var cartProductDocuments = emptyList<DocumentSnapshot>()
 
