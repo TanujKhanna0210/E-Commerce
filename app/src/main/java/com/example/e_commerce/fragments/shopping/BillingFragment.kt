c37ef11065ca59aacb037308a4c7e077c02ced47
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-11-12 03:49:23 +0530

    Layouts and adapters for billing fragment

---
+++ app/src/main/java/com/example/e_commerce/fragments/shopping/BillingFragment.kt
@@ -0,0 +1,49 @@
+package com.example.e_commerce.fragments.shopping
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.e_commerce.adapters.AddressAdapter
+import com.example.e_commerce.adapters.BillingProductsAdapter
+import com.example.e_commerce.databinding.FragmentBillingBinding
+
+class BillingFragment: Fragment() {
+    private lateinit var binding: FragmentBillingBinding
+    private val addressAdapter by lazy { AddressAdapter() }
+    private val billingProductsAdapter by lazy { BillingProductsAdapter() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBillingBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        setupBillingProductsRv()
+        setupAddressRv()
+    }
+
+    private fun setupAddressRv() {
+        binding.rvAddress.apply {
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+            adapter = addressAdapter
+        }
+    }
+
+    private fun setupBillingProductsRv() {
+        binding.rvProducts.apply {
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+            adapter = billingProductsAdapter
+        }
+    }
+
+}
