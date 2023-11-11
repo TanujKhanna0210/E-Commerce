daf0bb94bf98b8d27a2e66af87162f88c73c88f0
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-11-12 00:05:47 +0530

    Address fragment layout

---
+++ app/src/main/java/com/example/e_commerce/fragments/shopping/AddressFragment.kt
@@ -0,0 +1,21 @@
+package com.example.e_commerce.fragments.shopping
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.e_commerce.databinding.FragmentAddressBinding
+
+class AddressFragment: Fragment() {
+    private lateinit var binding: FragmentAddressBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddressBinding.inflate(inflater)
+        return binding.root
+    }
+}
