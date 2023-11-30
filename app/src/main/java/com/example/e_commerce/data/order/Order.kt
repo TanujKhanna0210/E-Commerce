ada391527875b2176cbd0afd3ac898f99936c1a2
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-11-30 13:46:56 +0530

    Setting up orders adapter

--- app/src/main/java/com/example/e_commerce/data/order/Order.kt
+++ app/src/main/java/com/example/e_commerce/data/order/Order.kt
@@ -2,10 +2,16 @@ package com.example.e_commerce.data.order
 
 import com.example.e_commerce.data.Address
 import com.example.e_commerce.data.CartProduct
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import kotlin.random.Random.Default.nextLong
 
 data class Order(
     val orderStatus: String,
     val totalPrice: Float,
     val products: List<CartProduct>,
-    val address: Address
+    val address: Address,
+    val date: String = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).format(Date()),
+    val orderId: Long = nextLong(0,100_000_000_000) + totalPrice.toLong()
 )
