8fdf79bb6d7ba2ac0782db316d767296f1b18e9c
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-11-30 15:25:24 +0530

    Setting up order detail fragment

--- app/src/main/java/com/example/e_commerce/fragments/settings/AllOrdersFragment.kt
+++ app/src/main/java/com/example/e_commerce/fragments/settings/AllOrdersFragment.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.e_commerce.adapters.AllOrdersAdapter
@@ -59,6 +60,12 @@ class AllOrdersFragment: Fragment() {
                 }
             }
         }
+
+        ordersAdapter.onClick = {
+            val action = AllOrdersFragmentDirections.actionAllOrdersFragmentToOrderDetailFragment(it)
+            findNavController().navigate(action)
+        }
+
     }
 
     private fun setupOrdersRv() {
