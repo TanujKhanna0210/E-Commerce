3bcb4cef4f06f0e623022f9e1e48c47848d931cf
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-12-01 19:03:32 +0530

    Minor fixes

--- app/src/main/java/com/example/e_commerce/adapters/SpecialProductsAdapter.kt
+++ app/src/main/java/com/example/e_commerce/adapters/SpecialProductsAdapter.kt
@@ -17,7 +17,7 @@ class SpecialProductsAdapter: RecyclerView.Adapter<SpecialProductsAdapter.Specia
             binding.apply {
                 Glide.with(itemView).load(product.images[0]).into(imageSpecialRvItem)
                 tvSpecialProductName.text = product.name
-                tvSpecialProductPrice.text = product.price.toString()
+                tvSpecialProductPrice.text = "$ ${product.price}"
             }
         }
     }
